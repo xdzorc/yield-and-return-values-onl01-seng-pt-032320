9711f217a474e61f90f89e2118f62ec65c17bf72
DAZUN XING <dazun.xing@gmail.com>
2020-04-02 02:16:13 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,21 @@
 require 'pry'
 
+# def hello(array)
+#   i = 0
+#   while i < array.length
+#   binding.pry
+#     yield(array[i])
+#     i += 1
+#   end
+# end
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-   binding.pry
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
 
